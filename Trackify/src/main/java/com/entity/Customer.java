b559aacd4d11c6928666a6710a7f781ad04b1e90
Ashish.Chaudhary <Ashish.Chaudhary@ENCDAPPUNLT0719>
2025-11-17 14:37:21 +0530

    added customer id

--- Trackify/src/main/java/com/entity/Customer.java
+++ Trackify/src/main/java/com/entity/Customer.java
@@ -1,5 +1,7 @@
 package com.entity;
 
+import java.util.Objects;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,8 +13,18 @@ import jakarta.persistence.Table;
 public class Customer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long uuid;
 	private String username;
 	private String password;
+	
+	
+	
+	public Long getUuid() {
+		return uuid;
+	}
+	public void setUuid(Long uuid) {
+		this.uuid = uuid;
+	}
 	public String getUsername() {
 		return username;
 	}
@@ -25,4 +37,26 @@ public class Customer {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	@Override
+	public String toString() {
+		return "Customer [uuid=" + uuid + ", username=" + username + ", password=" + password + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(password, username, uuid);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		return Objects.equals(password, other.password) && Objects.equals(username, other.username)
+				&& Objects.equals(uuid, other.uuid);
+	}
+	
+	
 }
